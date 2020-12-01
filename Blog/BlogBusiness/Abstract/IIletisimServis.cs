cf3e91654247bf1f1015f671b49fc793e75390ff
Talha YÜKSEK <yuksek_talha@hotmail.com>
2020-12-01 12:25:22 +0300

    s

--- Blog/BlogBusiness/Abstract/IIletisimServis.cs
+++ Blog/BlogBusiness/Abstract/IIletisimServis.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace BlogBusiness.Abstract
 {
-    class IIletisimServis
+   public class IIletisimServis
     {
+        List<Iletisim> IletisimBilgileriniGetir();
+        List<Iletisim> IletisimGetir(int IletisimId);
+        void Ekle(Iletisim iletisim);
+        void Guncelle(Iletisim iletisim);
+        void Sil(int id);
     }
 }
